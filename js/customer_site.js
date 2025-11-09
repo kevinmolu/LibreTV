1532711b2497747af830ccd04534500a3ab635e1
kevinmolu <125862621+kevinmolu@users.noreply.github.com>
2025-11-09 20:44:20 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -4,6 +4,27 @@ const CUSTOMER_SITES = {
         name: '溪桥资源',
     }
 };
+ // 可以继续添加更多站点
+    mzzy: {
+        api: 'https://mozhuazy.com/api.php/provide/vod',
+        name: '魔爪资源',
+        type: 'json',
+        searchable: 1,
+        quickSearch: 1,
+        filterable: 1
+    },
+    ffzy: {
+        api: 'https://api.feifeicms.org/api.php/provide/vod',
+        name: '飞飞资源',
+        type: 'json',
+        searchable: 1,
+        quickSearch: 1,
+        filterable: 1
+    }
+};
+
+
+
 
 // 调用全局方法合并
 if (window.extendAPISites) {
